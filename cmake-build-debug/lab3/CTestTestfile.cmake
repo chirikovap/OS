50f07aa89bb5d24b6eaeb94a70f6c719ea17e9e1
Polina Chirikova <polia.chirikova@ya.ru>
2023-01-04 22:45:10 +0400

    ND

--- cmake-build-debug/lab3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/polina/OS/lab3
-# Build directory: /home/polina/OS/cmake-build-debug/lab3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
